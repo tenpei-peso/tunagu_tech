5df7523cd48c4db377eb6ad2723fc389411aeb8a
tenpei-sano <kikichan.fps@gmail.com>
2025-10-12 20:00:10 +0900

    UIでの画面切り替えを終了、go_routerでリダイレクトするように変更

--- lib/root/presentation/root_screen.dart
+++ lib/root/presentation/root_screen.dart
@@ -1,62 +1,56 @@
-// Flutter imports:
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:go_router/go_router.dart';
 
-// Package imports:
-import 'package:hooks_riverpod/hooks_riverpod.dart';
+class RootScreen extends StatelessWidget {
+  // StatefulNavigationShellを受け取るように変更
+  const RootScreen({required this.navigationShell, super.key});
 
-// Project imports:
-import '../../features/authentication/presentation/welcme_screen.dart';
-import '../../features/authentication/provider/auth_state.dart';
-import '../../features/authentication/provider/auth_state_provider.dart';
-import 'tab_screen.dart';
-
-// Package imports:
-
-class RootScreen extends ConsumerWidget {
-  const RootScreen({super.key});
+  final StatefulNavigationShell navigationShell;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final authState = ref.watch(authNotifierProvider);
-
-    return authState.when(
-      // 未認証（初期状態も含む）
-      unauthenticated: (loading) {
-        if (loading) {
-          return const Scaffold(
-            body: Center(child: CircularProgressIndicator()),
-          );
-        }
-        return const WelcomeScreen();
-      },
-
-      // ローディング中
-      loading: () => const Scaffold(
-        body: Center(child: CircularProgressIndicator()),
-      ),
-
-      // 認証済み → TabScreenへ
-      authenticated: (user) => const TabScreen(),
-
-      // 認証失敗
-      failure: (message) => Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text('エラー: $message'),
-              const SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: () {
-                  // 再試行とかログアウトなど
-                  ref.read(authNotifierProvider.notifier).signOut();
-                },
-                child: const Text('戻る'),
-              ),
-            ],
+  Widget build(BuildContext context) => Scaffold(
+      // bodyにはnavigationShellを渡す
+      body: navigationShell,
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'ホーム',
           ),
-        ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: '検索',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message),
+            label: 'メッセージ',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'プロフィール',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: '設定',
+          ),
+        ],
+        // タブインデックスはnavigationShellから取得できる
+        currentIndex: navigationShell.currentIndex,
+        onTap: (index) => _onItemTapped(index, context),
       ),
+    );
+
+  // タップされた時の処理
+  void _onItemTapped(int index, BuildContext context) {
+    // タブ間の遷移はこのように行う
+    navigationShell.goBranch(
+      index,
+      // 第二引数にtrueを渡すと、タブのトップルートに遷移する
+      // つまり以下のようにすると、非アクティブなタブ（現在のタブとは別のタブ）をクリック
+      // した場合には状態を保ったままそのタブに遷移し、アクティブなタブ（現在のタブ）を
+      // クリックした場合にはトップのルートに戻るという挙動になる
+      initialLocation: index == navigationShell.currentIndex,
     );
   }
 }
