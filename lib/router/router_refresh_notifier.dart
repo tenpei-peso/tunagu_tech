a53f7043113916b2085108ea1ab83f72ec9cf61d
tenpei-sano <kikichan.fps@gmail.com>
2025-10-12 19:59:09 +0900

    rootを設定

---
+++ lib/router/router_refresh_notifier.dart
@@ -0,0 +1,16 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+
+class GoRouterRefreshNotifier extends ChangeNotifier {
+  GoRouterRefreshNotifier(Stream<dynamic> stream) {
+    _sub = stream.listen((_) => notifyListeners());
+  }
+  late final StreamSubscription _sub;
+
+  @override
+  void dispose() {
+    _sub.cancel();
+    super.dispose();
+  }
+}
