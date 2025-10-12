57f0298b7c9943a6f5a74128e1beaf49cc8641bf
tenpei-sano <kikichan.fps@gmail.com>
2025-10-12 20:22:09 +0900

    ボトムバーUI作成

--- lib/root/presentation/root_screen.dart
+++ lib/root/presentation/root_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+import '../../theme/Tunagu_colors.dart';
 
 class RootScreen extends StatelessWidget {
   // StatefulNavigationShellを受け取るように変更
@@ -12,44 +15,60 @@ class RootScreen extends StatelessWidget {
   Widget build(BuildContext context) => Scaffold(
       // bodyにはnavigationShellを渡す
       body: navigationShell,
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'ホーム',
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 20,
+              color: Colors.black.withValues(alpha: 0.1),
+            )
+          ],
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+            child: GNav(
+              rippleColor: TunaguColors.gray300,
+              hoverColor: TunaguColors.gray100,
+              gap: 8,
+              activeColor: TunaguColors.gray900,
+              iconSize: 24,
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 12),
+              duration: const Duration(milliseconds: 400),
+              tabBackgroundColor: TunaguColors.gray100,
+              color: TunaguColors.gray900,
+              tabs: const [
+                GButton(
+                  icon: Icons.home,
+                  text: 'Home',
+                ),
+                GButton(
+                  icon: Icons.search,
+                  text: 'Search',
+                ),
+                GButton(
+                  icon: Icons.message,
+                  text: 'Messages',
+                ),
+                GButton(
+                  icon: Icons.settings,
+                  text: 'Settings',
+                ),
+              ],
+              selectedIndex: navigationShell.currentIndex,
+              onTabChange: (index) {
+                _onItemTapped(index, context);
+              },
+            ),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: '検索',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.message),
-            label: 'メッセージ',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'プロフィール',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: '設定',
-          ),
-        ],
-        // タブインデックスはnavigationShellから取得できる
-        currentIndex: navigationShell.currentIndex,
-        onTap: (index) => _onItemTapped(index, context),
-      ),
+        ),
+    ),
     );
 
-  // タップされた時の処理
   void _onItemTapped(int index, BuildContext context) {
-    // タブ間の遷移はこのように行う
     navigationShell.goBranch(
       index,
-      // 第二引数にtrueを渡すと、タブのトップルートに遷移する
-      // つまり以下のようにすると、非アクティブなタブ（現在のタブとは別のタブ）をクリック
-      // した場合には状態を保ったままそのタブに遷移し、アクティブなタブ（現在のタブ）を
-      // クリックした場合にはトップのルートに戻るという挙動になる
       initialLocation: index == navigationShell.currentIndex,
     );
   }
